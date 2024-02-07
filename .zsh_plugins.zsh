7d96c8c626fb57a791746afbd8cb566bfbe2a5f5
Simone Rosani <rsn.smn@gmail.com>
2024-02-07 11:55:20 +0100

    added syntax higlighting to prompt

--- .zsh_plugins.zsh
+++ .zsh_plugins.zsh
@@ -1,2 +1,4 @@
 fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
 source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
