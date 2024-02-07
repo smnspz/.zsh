ecd177a5576b2ff28497a73305eba372b6700960
Simone Rosani <rsn.smn@gmail.com>
2024-02-07 13:12:57 +0100

    feat: added auto pair plugin

--- .zsh_plugins.zsh
+++ .zsh_plugins.zsh
@@ -2,3 +2,6 @@ fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.
 source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
 fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
 source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair )
+source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/autopair.plugin.zsh
+source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/zsh-autopair.plugin.zsh
