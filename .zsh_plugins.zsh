eba2481e81cd94037bac892c3069a4d2d489d6aa
Simone Rosani <rsn.smn@gmail.com>
2024-02-07 11:45:29 +0100

    added antidote .zsh_plugins files

---
+++ .zsh_plugins.zsh
@@ -0,0 +1,2 @@
+fpath+=( /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /Users/rosani/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
